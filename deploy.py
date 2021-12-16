bc7e8a593609269728c97e4dfacb9a62a4a07568
joebywan <joseph.d.howe@gmail.com>
2021-12-16 01:22:26 +0000

    working on efs script

--- deploy.py
+++ deploy.py
@@ -4,21 +4,11 @@ import boto3
 # defaultvpc = str(os.system('aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[*].VpcId" --output text'))
 from subprocess import PIPE, run
 
+#function to take os command output and return it.  To be used in other functions or variables.  Eliminates the error code return fouling up the return.
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
 
+#contains the default VPC id
 defaultvpc = out('aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[*].VpcId" --output text')
-print(type(defaultvpc))
-print("defaultVPC id is " + defaultvpc)
-print("|" + defaultvpc + "|")
 
-# ec2 = boto3.client('ec2')
-# filters = [{'Name':'isDefault','Values'='true'}]
-# default_vpc_id = ec2.describe_vpcs()
-# print(default_vpc_id)
-
-#default_vpc_id=$(aws ec2 describe-vpcs \
-#    --filters Name=isDefault,Values=true \
-#    --query 'Vpcs[*].VpcId' \
-#    --output text)
