02f38f9bc0e1badee9ca9c709c98af3c28621042
joeby wan <joseph.d.howe@gmail.com>
2024-02-04 19:53:23 +1000

    importing lambda log group

--- palworld/cloudwatch.tf
+++ palworld/cloudwatch.tf
@@ -4,15 +4,23 @@ resource "aws_cloudwatch_log_group" "ecs_log_group_server" {
   retention_in_days = 3
 }
 
-moved {
-  from = aws_cloudwatch_log_group.ecs_log_group
-  to   = aws_cloudwatch_log_group.ecs_log_group_server
-}
-
 # Log group for the ECS logs to be sent to
 resource "aws_cloudwatch_log_group" "ecs_log_group_watchdog" {
   name              = "/ecs/${local.workload_name}-watchdog"
   retention_in_days = 3
+}
+
+# Log group for the Discord notification logs to be sent to
+resource "aws_cloudwatch_log_group" "discord_notifications" {
+  provider = aws.us-east-1
+  name              = "/aws/lambda/${aws_lambda_function.discord_notifications.function_name}"
+  retention_in_days = 3
+}
+
+import {
+  provider = aws.us-east-1
+  to = aws_cloudwatch_log_group.discord_notifications
+  id = "/aws/lambda/${aws_lambda_function.discord_notifications.function_name}"
 }
 
 # Log group for the route53 query logs to be sent to
